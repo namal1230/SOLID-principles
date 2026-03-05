0dab9a5ebe8f32305856e56fde62e82675180f4e
Namal Dilmith Ruwanpathirana <prowaddosinhalatutorial@gmail.com>
2026-03-05 11:36:43 +0530

    single responsibility implemenation

--- SingleResponsibility/src/main/java/org/example/bestPractice/Main.java
+++ SingleResponsibility/src/main/java/org/example/bestPractice/Main.java
@@ -3,6 +3,13 @@ package org.example.bestPractice;
 import static org.example.bestPractice.DatabaseConnection.databaseConnection;
 import static org.example.bestPractice.NotificationHandle.showNotifications;
 
+// A class should have only one reason to change.
+
+/*
+    That means:
+        One class → One responsibility
+        If multiple responsibilities exist → It violates SRP
+*/
 public class Main {
     public static void main(String[] args){
         String database = databaseConnection();
